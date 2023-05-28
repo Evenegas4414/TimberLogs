d63a8d25363a334cbee5f4b555dc9007a62443dd
Exequiel Venegas <evenegas4414@gmail.com>
2023-05-28 18:11:28 -0400

    Init commit
    - Individual 8

---
+++ app/src/main/java/com/ervr/timber/MainActivity.java
@@ -0,0 +1,35 @@
+package com.ervr.timber;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import timber.log.Timber;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Timber.d("onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Timber.d("onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Timber.d("onResume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Timber.d("onDestroy");
+    }
+}
